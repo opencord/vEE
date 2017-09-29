a2962ec4890772028694d71813254434a6552403
Max Chu <maxmchu@gmail.com>
2017-09-29 11:49:29 -0700

    import syncstep, integrate 6094 fixing config error in header.py
    Change-Id: I434795e2058c1dcdca82c6f8d6a734d924e26d06
    (cherry picked from commit 5484b546a1fbef170e3f50f15d46a0963f7cfafb)

--- xos/header.py
+++ xos/header.py
@@ -19,7 +19,7 @@ from django.db.models import *
 from core.models import Service
 import traceback
 from xos.exceptions import *
-from xos.config import Config
+from xosconfig import Config
 
 SERVICE_NAME = 'vee'
 SERVICE_NAME_VERBOSE = 'VEE Service'
